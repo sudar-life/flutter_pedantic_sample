8fbc5cff8ff3c1ff13e18b5b3cc9dd1d87df7261
sudar-life <mousai86@gmail.com>
2021-04-18 20:37:06 +0900

    error rule 에대한 테스트 sample

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,86 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_pendantic_sample/main.dart';
+import 'package:flutter_pendantic_sample/model/a.dart';
+import '../lib/model/b.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  test('avoid_relative_lib_imports', () {
+    var aClass = A();
+    var bClass = B();
+  });
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  test('avoid_types_as_parameter_names', () {
+    void someFunction(int) {}
+    var value = 1;
+    someFunction(value);
+  });
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+  test('no_duplicate_case_values', () {
+    var a = 2;
+    switch (a) {
+      case 1:
+        print('1!!');
+        break;
+      case 2:
+        print('2!!');
+        break;
+      case 3:
+        print('3!!');
+        break;
+      case 2:
+        print('2222!!');
+        break;
+    }
+    var country = CounTryCodeEnumSample.AD;
+    switch (country) {
+      case CounTryCodeEnumSample.AD:
+      case CounTryCodeEnumSample.AE:
+      case CounTryCodeEnumSample.AF:
+      case CounTryCodeEnumSample.AG:
+      case CounTryCodeEnumSample.AI:
+      case CounTryCodeEnumSample.AL:
+      case CounTryCodeEnumSample.AM:
+      case CounTryCodeEnumSample.AN:
+      case CounTryCodeEnumSample.AO:
+      case CounTryCodeEnumSample.AQ:
+        break;
+    }
+  });
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  test('unrelated_type_equality_checks', () {
+    void someFunction() {
+      var x = '1';
+      if (x == 1) print('someFunction'); // LINT
+    }
+
+    void someFunction11(DerivedClass1 instance) {
+      var other = DerivedClass2();
+
+      if (other == instance) print('someFunction11'); // OK
+      if (other != instance) print('!someFunction11'); // OK
+    }
+
+    someFunction();
+    someFunction11(DerivedClass1());
+  });
+
+  test('valid_regexps', () {
+    // 정규표현식 역슬래시(\)는 확장 문자
+    //역슬래시 다음에 일반 문자가 오면 특수문자로 취급하고
+    //역슬래시 다음에 특수문자가 오면 그 문자 자체를 의미
+    //
+    // () 소괄호 안의 문자를 하나의 문자로 인식
+    print(RegExp(r'(').hasMatch('foo()'));
+    print(RegExp(r'\(').hasMatch('foo()'));
   });
 }
+
+class ClassBase {}
+
+abstract class Mixin {}
+
+class DerivedClass2 extends ClassBase with Mixin {}
+
+class DerivedClass1 {}
+
+enum CounTryCodeEnumSample { AD, AE, AF, AG, AI, AL, AM, AN, AO, AQ }
